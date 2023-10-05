416577ab6d72c9efb4a94732381f7f3fd10ed102
EMarvelM <marvelousegbe19@gmail.com>
2023-10-05 19:52:26 +0100

    if argv is more than 2(file_name and 1st argv) then print {d:0} followed by "arguments"

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,11 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
         print("{0:d} arguments.".format(len(argv) - 1))
-    elif len(argv) > 1:
+    elif len(argv) == 2:
         print("{0:d} argument:".format(len(argv) - 1))
+        for i in range(len(argv) - 1):
+            print("{0:d}: {1}".format(i + 1, argv[i + 1]))
+    elif len(argv) > 2:
+        print("{0:d} arguments:".format(len(argv) - 1))
         for i in range(len(argv) - 1):
             print("{0:d}: {1}".format(i + 1, argv[i + 1]))
