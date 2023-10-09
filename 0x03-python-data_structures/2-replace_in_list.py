81b125e345de285d8c95b39b1ca5fbdef4d0258a
EMarvelM <marvelousegbe19@gmail.com>
2023-10-09 11:44:41 +0100

    fnc to replaces an element of a list at a specific position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        myNew_list = my_list
+        myNew_list[idx] = element
+        return myNew_list   
+    else:
+        return my_list
