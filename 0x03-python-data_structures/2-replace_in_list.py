67798020bfb2491d926c4f6a63f6d3c4ac9df9f1
EMarvelM <marvelousegbe19@gmail.com>
2023-10-09 11:46:56 +0100

    remove trailing spece

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@ def replace_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):
         myNew_list = my_list
         myNew_list[idx] = element
-        return myNew_list   
+        return myNew_list
     else:
         return my_list
