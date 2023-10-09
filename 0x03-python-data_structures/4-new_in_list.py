47acb7fd625f73ef6cd64c9ab47f45ce86f66cb1
EMarvelM <marvelousegbe19@gmail.com>
2023-10-09 15:54:07 +0100

    fnc to replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        myNew_list = []
+        for i in range(len(my_list)):
+            if i == idx:
+                myNew_list.append(element)
+            else:
+                myNew_list.append(my_list[i])
+        return myNew_list
+    else:
+        return my_list
