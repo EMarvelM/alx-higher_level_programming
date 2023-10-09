122af01492137b7db7338ac7e5938e2b7c8f5427
EMarvelM <marvelousegbe19@gmail.com>
2023-10-09 16:58:29 +0100

    just a new line when matrix is empty

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
     for lis in matrix:
         for i in range(len(lis)):
             print(lis[i], end="")
