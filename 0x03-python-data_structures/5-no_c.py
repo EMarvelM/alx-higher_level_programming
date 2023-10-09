9bd1e256236c16140c9b549cb0a11d5086e517cb
EMarvelM <marvelousegbe19@gmail.com>
2023-10-09 16:08:45 +0100

    fnc to removes all characters c and C from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    myNew_str = ''
+    for c in my_string:
+        if c == "c" or c == "C":
+            pass
+        else:
+            myNew_str += c
+    return myNew_str
+
