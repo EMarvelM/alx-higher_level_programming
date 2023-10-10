7e269513bc61cad3fb6db34dc32741c56f5d1f8a
EMarvelM <marvelousegbe19@gmail.com>
2023-10-10 16:52:01 +0100

    add_tuple: If a tuple is bigger than 2, use only the first 2 integers

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,12 +3,19 @@ def add_tuple(tuple_a=(), tuple_b=()):
     lis = []
     tuple_a = list(tuple_a)
     tuple_b = list(tuple_b)
-    for i in range(len(tuple_a)):
-        try:
-            tuple_b[i] >= 0
-        except IndexError:
-            tuple_b.append(0)
-            
-        lis += [tuple_a[i] + tuple_b[i]]
-        tuple_c = tuple(lis)
+    if len(tuple_a) > len(tuple_b):
+        lent = len(tuple_a)
+    else:
+        lent = len(tuple_b)
+    for i in range(lent):
+        if i < 2:
+            try:
+                tuple_b[i] >= 0
+            except IndexError:
+                tuple_b.append(0)
+
+            lis += [tuple_a[i] + tuple_b[i]]
+            tuple_c = tuple(lis)
+        else:
+            pass
     return tuple_c
