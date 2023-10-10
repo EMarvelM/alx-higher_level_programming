1f459bcd8187cdbcdca21fce116748ec5c7cb618
EMarvelM <marvelousegbe19@gmail.com>
2023-10-10 16:35:15 +0100

    make sure to convert to list before modifing since tuple is immutable

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    lis = []
+    tuple_a = list(tuple_a)
+    tuple_b = list(tuple_b)
+    for i in range(len(tuple_a)):
+        try:
+            tuple_b[i] >= 0
+        except IndexError:
+            tuple_b.append(0)
+            
+        lis += [tuple_a[i] + tuple_b[i]]
+        tuple_c = tuple(lis)
+    return tuple_c
