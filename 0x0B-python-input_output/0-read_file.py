9ce88adedf8d0ed757f1b4d2f513af5d7b6e2e11
EMarvelM <marvelousegbe19@gmail.com>
2023-11-07 23:31:56 +0100

    added comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module defines a text file-reading function"""
+
 def read_file(filename=""):
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         for line in f:
             print(line, end="")
+
+
