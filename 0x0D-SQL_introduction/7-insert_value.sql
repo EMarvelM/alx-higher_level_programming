92e6f089db013b41faf10e61f508d4a9b650e245
EMarvelM <marvelousegbe19@gmail.com>
2024-01-18 22:25:12 +0100

    Specified the variable type

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,6 +1,6 @@
 -- inserts a new row in the table first_table
 
-INSERT INTO first_table(id, name) VALUES (
+INSERT INTO first_table(id INT, name VARCHAR(256)) VALUES (
     id = 89,
     name = "Best School"
 );
