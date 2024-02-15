790b441ecaebe182406db58d35e873b1ddc7a6cd
EMarvelM <marvelousegbe19@gmail.com>
2024-02-15 14:57:59 +0100

    :[ semistandard -

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,22 +2,18 @@
 const { argv } = require('node:process');
 
 let num = Number(argv[2]);
-if (isNaN(num))
-{
-	num = 1;
-	console.log(num)
+if (isNaN(num)) {
+  num = 1;
+  console.log(num);
 } else {
   // calculate factorial
   num = factor(num);
   console.log(num);
 }
 
-function factor(n) {
-  if (n < 1) {
-    return 1;
-  }
-  let num = n - 1;
-  let ret;
-  ret = factor(num);
+function factor (n) {
+  if (n < 1) return 1;
+  const num = n - 1;
+  const ret = factor(num);
   return Number(n * ret);
 }
