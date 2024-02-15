6a6238c88b5b735004a44e3cfea244c266b2a12c
EMarvelM <marvelousegbe19@gmail.com>
2024-02-15 13:54:13 +0100

    :( semistandard bn

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const lang = [
-	'C is fun',
-	'Python is cool',
-	'JavaScript is amazing'
-]
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
 for (let i = 0; i < lang.length; i++) {
- console.log(lang[i])
+  console.log(lang[i]);
 }
