5570b564f9d0993d273a3f25ef456b17d7d1b62b
EMarvelM <marvelousegbe19@gmail.com>
2024-02-17 09:52:49 +0100

    get the largest second number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,20 +7,28 @@ if (argv.length < 4) {
 } else {
   // get largest number
   let largest = Number(argv[2]);
-  let word;
-  argv.forEach((word)=> {
+  let smallest = Number(argv[2]);
+  argv.forEach((word) => {
     if (!(isNaN(Number(word)))) {
       // write condition to check for the largest number
       if (largest < Number(word)) {
-        largest =  Number(word);
+        largest = Number(word);
+      }
+      if (smallest > Number(word)) {
+        smallest = Number(word);
       }
     }
   });
 
-  // prrint the largest number
-  console.log(largest);
+  // get the second largest number
+  for (let i = 0; i < argv.length; i++) {
+    if (!(isNaN(Number(argv[i])))) {
+      if (Number(argv[i]) < largest && smallest < Number(argv[i])) {
+        smallest = Number(argv[i]);
+      }
+    }
+  }
+
+  // print the second largest number
+  console.log(smallest);
 }
-
-
-// node program_name argv0 argv1
-// ./pagram_name
