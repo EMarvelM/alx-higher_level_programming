d7e40e7a3d0c6b2e09df0e421bff7ae3b932e970
EMarvelM <marvelousegbe19@gmail.com>
2024-02-23 13:07:46 +0100

    Modified - 6-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,8 @@
 #!/usr/bin/node
-
-const Rectangle = require('./4-rectangle');
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
-    if (c !== undefined) {
-      for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
-      }
-    } else {
-      this.print();
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
     }
   }
 };
