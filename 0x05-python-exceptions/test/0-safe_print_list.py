618768901ea597726fe559ccbd67c802ebda1fd3
EMarvelM <marvelousegbe19@gmail.com>
2024-03-11 13:31:35 +0100

    rm dir cause of alx policies

--- 0x05-python-exceptions/test/0-safe_print_list.py
+++ 0x05-python-exceptions/test/0-safe_print_list.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+import random
+_list = ["Yes", "No"]
+for i in range(101):
+    coin = random.choice(_list)
+    print (coin)
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const { argv } = require('node:process');
-
-if (argv[2] == null) {
-  console.log('No argument');
-} else if (argv[2] !== 'undefined') {
-  console.log(process.argv[2]);
-}
