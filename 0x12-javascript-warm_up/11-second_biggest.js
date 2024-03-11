c1955256e922d3721bc2a14e326a897102884aa1
EMarvelM <marvelousegbe19@gmail.com>
2024-03-11 16:23:42 +0100

    accurate sorting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
+numbers = [];
+argv.forEach( (num) => numbers.push(Number(num)));
+
 if (argv[2]) {
-  console.log(argv.splice(2).sort().slice(-2)[0]);
-} else if (!isNaN(Number(argv[2]))) {
-console.log(0);
-}
+    console.log(numbers.splice(2).sort((a, b) => a - b).slice(-2)[0]);
+  } else if (!isNaN(Number(numbers[2]))) {
+    console.log(0);
+  }
