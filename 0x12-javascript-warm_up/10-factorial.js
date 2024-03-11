a101a49046360e9e95fcc857a5ed92ff55ca50c7
EMarvelM <marvelousegbe19@gmail.com>
2024-03-11 15:53:25 +0100

    semistandard, if no argument then output 1

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-
-const n = factorial(Number(argv[2]));
-console.log(n);
+if (!argv[2]) {
+  console.log(1);
+} else {
+  const n = factorial(Number(argv[2]));
+  console.log(n);
+}
 
 function factorial (n) {
   if (n < 2) {
