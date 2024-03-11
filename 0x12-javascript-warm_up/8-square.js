3c8315adbabe254683753955a6e9c5cacb300103
EMarvelM <marvelousegbe19@gmail.com>
2024-03-11 15:28:48 +0100

    semistandard: indent; eol-last

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 const { argv } = require('node:process');
 
 if (!isNaN(Number(argv[2]))) {
-    for (let i = 0; i < Number(argv[2]); i++) {
-        console.log('X'.repeat(Number(argv[2])));
-    }
+  for (let i = 0; i < Number(argv[2]); i++) {
+    console.log('X'.repeat(Number(argv[2])));
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
