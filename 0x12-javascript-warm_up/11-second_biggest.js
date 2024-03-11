4c77d0affddb9cd576c505a42c76b4014313cbf7
EMarvelM <marvelousegbe19@gmail.com>
2024-03-11 16:09:19 +0100

    on string not convertible to int then do not print

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 const { argv } = require('node:process');
 if (argv[2]) {
   console.log(argv.splice(2).sort().slice(-2)[0]);
-} else {
-  console.log(0);
+} else if (!isNaN(Number(argv[2]))) {
+console.log(0);
 }
