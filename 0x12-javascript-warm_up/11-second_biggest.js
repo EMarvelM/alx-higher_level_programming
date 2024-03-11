fc643923def2e6d112df1783738fe8eebccbbcd3
EMarvelM <marvelousegbe19@gmail.com>
2024-03-11 16:26:37 +0100

    If no argument passed, print 0If the number of arguments is 1, print 0

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,8 @@ const { argv } = require('node:process');
 numbers = [];
 argv.forEach( (num) => numbers.push(Number(num)));
 
-if (argv[2]) {
-    console.log(numbers.splice(2).sort((a, b) => a - b).slice(-2)[0]);
-  } else if (!isNaN(Number(numbers[2]))) {
-    console.log(0);
-  }
+if (argv[3]) {
+  console.log(numbers.splice(2).sort((a, b) => a - b).slice(-2)[0]);
+} else if (argv.length < 5 || !isNaN(Number(numbers[2]))) {
+  console.log(0);
+}
