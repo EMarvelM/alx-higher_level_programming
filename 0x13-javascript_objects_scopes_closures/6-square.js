90fcb57e0120abe9d668c238e7f8c2b874845414
EMarvelM <marvelousegbe19@gmail.com>
2024-03-12 17:28:48 +0100

    should not work if other than string

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ module.exports = class Square extends Squared {
   }
 
   charPrint(c) {
-    if (c) {
+    if (c && typeof c === 'string') {
       for (let i = 0; i < this.height; i++){
         console.log(c.repeat(this.width));
       }
