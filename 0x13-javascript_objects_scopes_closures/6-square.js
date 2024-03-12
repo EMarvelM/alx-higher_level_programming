0cda657d1227aea81375e3446f647b61703de602
EMarvelM <marvelousegbe19@gmail.com>
2024-03-12 17:45:03 +0100

    useless constructor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,17 +2,13 @@
 const Squared = require('./5-square');
 
 module.exports = class Square extends Squared {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c && typeof c === 'string') {
       for (let i = 0; i < this.height; i++) {
         console.log(c.repeat(this.width));
       }
     } else {
-        this.print();
+      this.print();
     }
   }
 };
