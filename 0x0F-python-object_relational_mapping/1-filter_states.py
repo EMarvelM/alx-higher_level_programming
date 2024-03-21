89c2ef2e9686d1e98accb79e55ed4bf1a70a0b45
EMarvelM <marvelousegbe19@gmail.com>
2024-03-21 13:24:16 +0100

    pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
+ Usage: ./0-select_states.py <mysql username>
+                            <mysql password>
+                            <database name >
+"""
+
 import MySQLdb
 from sys import argv
 
@@ -16,7 +23,8 @@ if __name__ == "__main__":
 
     c = db.cursor()
 
-    c.execute("SELECT * FROM states WHERE LEFT(name, 1) = 'N' ORDER BY(id) ASC")
+    c.execute("SELECT * FROM states WHERE LEFT(name, 1) \
+              = 'N' ORDER BY(id) ASC")
     [print(x) for x in c.fetchall()]
 
     c.close()
