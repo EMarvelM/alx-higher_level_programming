35663cb1c6950bebe4c12081d9dd1dee7b7b2860
EMarvelM <marvelousegbe19@gmail.com>
2024-03-21 13:42:42 +0100

    filter after getting the data

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -23,9 +23,8 @@ if __name__ == "__main__":
 
     c = db.cursor()
 
-    c.execute("SELECT * FROM states WHERE LEFT(name, 1) \
-              = 'N' ORDER BY(id) ASC")
-    [print(x) for x in c.fetchall()]
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
 
     c.close()
     db.close()
