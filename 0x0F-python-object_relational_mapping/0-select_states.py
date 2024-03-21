6d143c98434a87ab44754fc00bc751c794d55d0a
EMarvelM <marvelousegbe19@gmail.com>
2024-03-21 12:37:38 +0100

    execute SQL statement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,4 +2,10 @@
 import MySQLdb
 from sys import argv
 
+#.0-select_states.py root '' hbtn_0e_0_usa
 db = MySQLdb.Connect( host="localhost", user=argv[1], password=argv[2], database=argv[3] )
+c = db.cursor()
+
+c.execute("SELECT * FROM states")
+result = c.fetchall()
+
