f2d49f1460c6403140f7ce44b058ece4aa2413a6
EMarvelM <marvelousegbe19@gmail.com>
2024-03-21 13:04:18 +0100

    no argument validation needed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
