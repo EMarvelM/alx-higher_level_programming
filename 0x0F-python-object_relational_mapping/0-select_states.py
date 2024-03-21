5a8a6dddb0f6f37bc62f4e204233b2666775feed
EMarvelM <marvelousegbe19@gmail.com>
2024-03-21 12:39:46 +0100

    best practice

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,8 +2,13 @@
 import MySQLdb
 from sys import argv
 
-#.0-select_states.py root '' hbtn_0e_0_usa
-db = MySQLdb.Connect( host="localhost", user=argv[1], password=argv[2], database=argv[3] )
+# .0-select_states.py root '' hbtn_0e_0_usa
+db = MySQLdb.Connect(
+    host="localhost",
+    user=argv[1],
+    password=argv[2],
+    database=argv[3]
+)
 c = db.cursor()
 
 c.execute("SELECT * FROM states")
