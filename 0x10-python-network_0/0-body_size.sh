82a73244d7a8a25eba69656000dbc385c7198e08
emarvelm <marvelousegbe19@gmail.com>
2024-03-30 14:11:51 +0100

    takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Store the URL provided as input
-URL=$1
-
-# Send a request to the URL using curl and store the response body in a temporary file
-RESPONSE_FILE=$(mktemp)
-curl -s -o "$RESPONSE_FILE" "$URL"
-
-# Get the size of the response body in bytes
-BODY_SIZE=$(stat -c %s "$RESPONSE_FILE")
-
-# Display the size of the response body
-echo "$BODY_SIZE"
-
-# Clean up - remove the temporary file
-rm "$RESPONSE_FILE"
-
-
+#takes a URL, sends a request and displays size of the body of the response
+curl -s "$1" | wc -c
