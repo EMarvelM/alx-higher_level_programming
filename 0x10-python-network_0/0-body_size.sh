5b56211fc57151c284fd35f9abf9e8e0922125e3
emarvelm <marvelousegbe19@gmail.com>
2024-03-30 14:02:43 +0100

    still correct way to implement

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # take a url, send a request and displays the size of the body
 url=$1
-curl -w "%{size_download}" "$url"
+#curl -w "%{size_download}" "$url"
+curl -s "$url" | wc -c
+
 
