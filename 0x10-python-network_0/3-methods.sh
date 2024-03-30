2f0c889eb65d6e7bb5d56d8ed540ce8ef52ffc7c
emarvelm <marvelousegbe19@gmail.com>
2024-03-30 20:51:28 +0100

    now takes command line argument

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept.
-curl -Is 0.0.0.0:5000/route_4 | grep "Allow" | sed s/Allow:\ //
+curl -Is $1 | grep "Allow" | sed s/Allow:\ //
