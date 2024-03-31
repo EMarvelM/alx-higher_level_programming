3ae8e9616deb458958d2b017a7c5cd99da276466
emarvelm <marvelousegbe19@gmail.com>
2024-03-31 22:02:44 +0100

    pycodestyle resolved

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ module that finds a peak in a list of unsorted integers """
 
-def find_peak(list_of_integers:list):
+
+def find_peak(list_of_integers):
+
     """ function that finds a peak in a list of unsorted integers """
     if list_of_integers:
         list_of_integers.sort()
