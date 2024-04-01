64ca79981cdd418567e3bf753735f52ad1b595b7
emarvelm <marvelousegbe19@gmail.com>
2024-04-01 21:38:19 +0100

    takes a URL as a command-line argument, sends a request to that URL using urllib, and then retrieves the value of the X-Request-Id header from the response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,12 @@
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as data:
-    head = data.headers
-    print(head.get("X-Request-Id"))
+if len(sys.argv) != 2:
+    sys.exit(1)
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    x_request_id = response.getheader('X-Request-Id')
+    if x_request_id:
+        print(x_request_id)
