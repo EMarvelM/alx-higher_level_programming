30a2b0045d6d0e03ee8b4b205421565142404944
emarvelm <marvelousegbe19@gmail.com>
2024-04-01 20:59:59 +0100

    resolve pycodestyle

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header"""
+"""URL, sends request to the URL and displays the value of the X-Request-Id"""
 import urllib.request
 import sys
 
 with urllib.request.urlopen(sys.argv[1]) as data:
     head = data.headers
     print(head.get("X-Request-Id"))
-
-
