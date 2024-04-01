01f1a8510604c3d78e7729b6395d098f969ac440
emarvelm <marvelousegbe19@gmail.com>
2024-04-01 21:48:43 +0100

    should not run if imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,12 +3,13 @@
 import urllib.request
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    x_request_id = response.getheader('X-Request-Id')
-    if x_request_id:
-        print(x_request_id)
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.getheader('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
