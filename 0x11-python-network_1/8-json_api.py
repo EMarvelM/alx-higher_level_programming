53ad7a6b39cc6b8e8520a47dd1e4fb9b1479c85f
emarvelm <marvelousegbe19@gmail.com>
2024-04-02 18:46:42 +0100

    when json is not valid print as follow

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,12 +13,10 @@ if __name__ == "__main__":
         data = {"q": ""}
     re = requests.post("http://0.0.0.0:5000/search_user", data=data)
     _data = re.json()
-
-    if _data and (isinstance(_data, dict)):
-        print("[{}] {}".format(_data.get('id'), _data.get('name')))
-
-    elif _data and not isinstance(_data, dict):
+    try:
+        if _data and (isinstance(_data, dict)):
+            print("[{}] {}".format(_data.get('id'), _data.get('name')))
+        else:
+            print("No result")
+    except json.decoder.JSONDecoderError:
         print("Not a valid JSON")
-
-    else:
-        print("No result")
