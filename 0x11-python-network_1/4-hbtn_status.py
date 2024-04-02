048aec25e57abdad6a916ad6a3afac11b8dabdb6
emarvelm <marvelousegbe19@gmail.com>
2024-04-02 12:28:46 +0100

    resolved pycodestyle

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,7 @@ if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
     content = r.content.decode("utf-8")
     c_type = type(content)
-    _str = "Body response:\n\t- type: {}\n\t- content: {}".format(c_type, content)
+    _str = "Body response:\
+\n\t- type: {}\
+\n\t- content: {}".format(c_type, content)
     print(_str)
