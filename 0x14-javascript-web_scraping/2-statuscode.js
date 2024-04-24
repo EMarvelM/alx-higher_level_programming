b409befcbf6f3012cd6fa44c0420cb6af3e37175
emarvelm <marvelousegbe19@gmail.com>
2024-04-24 14:20:43 +0100

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,14 +3,13 @@
 const request = require('request');
 const { argv } = require('node:process');
 
-url = argv[2];
-console.log(url);
-if (!url) { process.exit(1); }
+const url = argv[2];
+// if (!url) { process.exit(1); }
 
 request(url, (err, response, body) => {
   if (err) {
     console.log(err);
     return;
-  };
+  }
   console.log('code: ', response.statusCode);
-})
+});
