016ac8eb84d5f4d7bf8e284c219b06c61d8ef011
emarvelm <marvelousegbe19@gmail.com>
2024-04-24 13:35:36 +0100

    script that writes a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+const { writeFile } = require('node:fs');
+
+const file = argv[2];
+const content = argv[3];
+
+writeFile(file, content, (err) => {
+  if (err) throw err;
+})
+
